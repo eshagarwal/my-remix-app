9683d3c87ec1874a2cd8eeaf7d7d015a1262ec18
eshagarwal <esha.agarwal0212@gmail.com>
2024-03-12 10:37:49 +0000

    styles

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,12 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction , LinksFunction } from "@remix-run/node";
+// eslint-disable-next-line import/no-unresolved
+import stylesUrl from "~/styles/index.css";
+import { Link } from "@remix-run/react";
+
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesUrl }
+];
 
 export const meta: MetaFunction = () => {
   return [
@@ -9,33 +17,19 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="container">
+      <div className="content">
+        <h1>
+          Remix <span>Jokes!</span>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="jokes">Read Jokes</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 }
---
+++ app/routes/jokes.$jokeId.tsx
@@ -0,0 +1,12 @@
+export default function JokeRoute() {
+    return (
+      <div>
+        <p>Heres your hilarious joke:</p>
+        <p>
+          Why dont you find hippopotamuses hiding in trees?
+          Theyre really good at it.
+        </p>
+      </div>
+    );
+  }
+  
